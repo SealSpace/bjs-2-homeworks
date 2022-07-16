6c9036f0d0ee7c970642659688f45df4967e70fe
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-07-16 13:28:29 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,21 @@
+"use strict";
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
+      let sqroot1,sqroot2;
+      let d = Math.pow (b,2)-4*a*c;
+          if (d > 0) {
+              sqroot1 = (-b + Math.sqrt(d) )/(2*a);
+              sqroot2 = (-b - Math.sqrt(d) )/(2*a);
+              arr = [sqroot1,sqroot2]
+          } 
+          else if (d == 0) {
+              sqroot1 = -b/(2*a);
+              arr = [sqroot1]
+          }
+          else if (d < 0) {
+              arr = []
+          }
+    }
   return arr; // array
 }
 
