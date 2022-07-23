0a4e8e4255f93e444f30507641d80d0c4d7e134e
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-07-23 13:58:08 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,9 +1,20 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+  let avg = 0;
   // Ваш код
-
+for (let i = 0; i < arr.length; i++) {
+  sum = sum + arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+}
+  avg = parseFloat((sum / arr.length).toFixed(2));
   return { min: min, max: max, avg: avg };
 }
 
