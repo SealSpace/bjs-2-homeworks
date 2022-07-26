ea33b0996a80ce98299004a76e042f6ab7368065
SealSpace <106908062+SealSpace@users.noreply.github.com>
2022-07-27 00:09:38 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+  function everyCompare(index) {
+
+    return arr1[index] === arr2[index];
+
+  }
+  result = arr1.length === arr2.length && arr1.every(everyCompare)
 
   return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+  let resultArr = arr
+  .filter((arr) => arr > 0)
+  .filter((arr) => arr % 3 === 0)
+  .map((arr) => arr * 10);
 
   return resultArr; // array
 }
